b515ab75b7c15deff448cdcd4ee43abc54db9a0c
nbaldzhiev <nenko.baldzhiev@gmail.com>
2021-09-22 17:46:56 +0300

    add ingredients

--- js/index.js
+++ js/index.js
@@ -5,6 +5,15 @@ async function getRandomCocktail() {
 }
 
 function getCocktailHtml(cocktail) {
+    let ingredients = []
+    Object.keys(cocktail).filter((key) => {
+        if (key.startsWith('strIngredient')) {
+            if (cocktail[key]) {
+                ingredients.push(cocktail[key])
+            }
+        }
+    })
+    console.log(ingredients.join(''))
     return `
         <div class='cocktail-item'>
             <img class='cocktail-thumbnail' src='${cocktail.strDrinkThumb}'></img>
@@ -12,7 +21,8 @@ function getCocktailHtml(cocktail) {
                 <p class='cocktail-name'>Name ... ${cocktail.strDrink}</p>
                 <p class='cocktail-category'>Category ... ${cocktail.strCategory}</p>
                 <p class='cocktail-glass'>Drink from ... ${cocktail.strGlass}</p>
-                <p class='cocktail-instructions'>Instructions ... ${cocktail.strInstructions}</p>
+                <p class='ingredients'>Ingredients ... <span class='ingredient-items'>${ingredients.join(', ')}</span></p>
+                <p class='instructions'>Instructions ... ${cocktail.strInstructions}</p>
             </div>
         </div>
     `
